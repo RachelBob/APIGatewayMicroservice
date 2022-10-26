ad3918c9e76461c2e2eea9daa2ee73ed920818ea
RachelBob <bobbyrachel@gmail.com>
2022-10-26 22:31:02 +0530

    CORS Added

--- src/main/java/com/lti/apigateway/security/WebFluxSecurityConfig.java
+++ src/main/java/com/lti/apigateway/security/WebFluxSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -12,10 +15,21 @@ public class WebFluxSecurityConfig {
 
 	@Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
-		
 		http
 		.csrf(csrf -> csrf.disable());
 		
-    return http.build();
+		return http.build();
+    }
+	
+	@Bean
+    public WebFluxConfigurer corsConfigurer() {
+        return new WebFluxConfigurerComposite() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*");
+            }
+        };
     }
 }
