bbcb0525031e8cabd1f5d86949047d121d17d306
RachelBob <bobbyrachel@gmail.com>
2022-10-27 13:29:47 +0530

    Cors related changes

--- src/main/java/com/lti/apigateway/security/WebFluxSecurityConfig.java
+++ src/main/java/com/lti/apigateway/security/WebFluxSecurityConfig.java
@@ -5,9 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
-import org.springframework.web.reactive.config.WebFluxConfigurerComposite;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -22,14 +22,16 @@ public class WebFluxSecurityConfig {
     }
 	
 	@Bean
-    public WebFluxConfigurer corsConfigurer() {
-        return new WebFluxConfigurerComposite() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("*")
-                        .allowedMethods("*");
-            }
-        };
-    }
+	CorsWebFilter corsFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		//corsConfig.setAllowCredentials(true);
+		corsConfig.addAllowedOrigin("http://localhost:4200");
+		corsConfig.addAllowedMethod("*");
+		corsConfig.addAllowedHeader("*");
+		
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+		
+		return new CorsWebFilter(source);
+	}
 }
